e32ba95a6a4e3252b9c3f9954ed6e5c1c5e37601
Naomi <33375791+Henvy-Mango@users.noreply.github.com>
2023-03-22 17:59:08 +0800

    feat: day 10

---
+++ note/LinkList/10-day-876.链表的中间结点.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=876 lang=java
+ *
+ * [876] 链表的中间结点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        // 快慢指针初始化指向 head
+        ListNode slow = head, fast = head;
+        // 快指针走到末尾时停止
+        while (fast != null && fast.next != null) {
+            // 慢指针走一步，快指针走两步
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // 慢指针指向中点
+        // 如果链表长度为偶数，也就是说中点有两个的时候，这个解法返回的节点是靠后的那个节点。
+        return slow;
+    }
+}
+// @lc code=end
+
